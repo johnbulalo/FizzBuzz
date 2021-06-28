c2332fd43f217620e78946a2fe5be4fdc7857ff8
John Paul Erica <jpaulerica998@gmail.com>
2021-06-28 15:26:33 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //bool fizz,buzz;
+            int fiz, buz;
+
+            for (int i = 1; i<100; i++) {
+                fiz = i % 3;
+                buz = i % 5;
+
+                if (fiz == 0 && buz == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else
+                {
+                    if (fiz == 0)
+                    {
+                        Console.WriteLine("Fizz");
+                    }
+                    else if(buz == 0)
+                    {
+                        Console.WriteLine("Buzz");
+                    }
+                    else
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+
+            }
+            
+        }
+    }
+}
